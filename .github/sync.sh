cf4e80a21bad65fae75c4daf81532c3dd07e0a1c
yun <yunyunpp96@outlook.com>
2024-01-09 22:49:53 +0800

    update shaders

--- .github/sync.sh
+++ .github/sync.sh
@@ -2,16 +2,33 @@
 tmp=$(mktemp -d)
 
 repo=$1             # source repo
-from=$2             # used files
 
-if [ ! $3 ]; then
-    to=$from
+if [[ "$2" =~ ^@ ]]; then
+    branch=$2           # repo branch
+    from=$3             # used files
+    to=$4
 else
+    from=$2
     to=$3
 fi
 
-git clone https://github.com/$repo $tmp --depth 1
+echo "$branch"
 
+if [ ! $to ]; then
+    to=$from
+fi
+
+if [[ $branch == '@gist' ]]; then
+    curl -o "$tmp/$from" "https://gist.githubusercontent.com/$repo/raw/$from"
+else
+    if [ ! $branch ]; then
+        git clone --depth 1 https://github.com/$repo $tmp 
+    else
+        git clone --depth 1 --branch ${branch:1} https://github.com/$repo $tmp
+    fi
+fi
+
+mkdir -p $to
 cp $tmp/$from $to
 
 rm -rf $tmp
