771c8a4ed92b63c337614d3cedd4896b7690b2ea
yun <yunyunpp96@outlook.com>
2024-01-14 15:59:49 +0800

    explicitly require module

--- scripts/rotate-video.lua
+++ scripts/rotate-video.lua
@@ -1,19 +1,22 @@
 -- source get from: https://github.com/mpv-player/mpv/issues/3869#issuecomment-264771991
 -- Rotates the video while maintaining 0 <= prop < 360
-function rotate_video(amt)
-	-- Ensure that amount is a base 10 integer.
-	amt = tonumber(amt, 10)
-	if amt == nil then
-		mp.osd_message("Rotate: Invalid rotation amount")
-		return nil -- abort
-	end
-	-- Calculate what the next rotation value should be,
-	-- and wrap value to correct range (0 (aka 360) to 359).
-	local rot = mp.get_property_number("video-rotate")
-	rot = ( rot + amt ) % 360
-	-- Change rotation and tell the user.
-	mp.set_property_number("video-rotate", rot)
-	mp.osd_message("Rotate: " .. rot)
+
+local mp = require 'mp'
+
+local function rotate_video(amt)
+    -- Ensure that amount is a base 10 integer.
+    amt = tonumber(amt, 10)
+    if amt == nil then
+        mp.osd_message("Rotate: Invalid rotation amount")
+        return nil -- abort
+    end
+    -- Calculate what the next rotation value should be,
+    -- and wrap value to correct range (0 (aka 360) to 359).
+    local rot = mp.get_property_number("video-rotate")
+    rot = (rot + amt) % 360
+    -- Change rotation and tell the user.
+    mp.set_property_number("video-rotate", rot)
+    mp.osd_message("Rotate: " .. rot)
 end
 
 mp.register_script_message("rotate-video", rotate_video)
